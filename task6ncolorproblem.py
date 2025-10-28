d4511f099ec626e6a4013fdee7d382927bcccdbd
vtu28363-create <vtu28363@veltech.edu.in>
2025-10-28 12:32:11 +0530

    Add files via upload

---
+++ task6ncolorproblem.py
@@ -0,0 +1,47 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/student/AppData/Local/Programs/Python/Python312/task8nqueenproblem.py
+
+
+= RESTART: C:/Users/student/AppData/Local/Programs/Python/Python312/ts8.py
+Graph 1 with 3 colors:
+Solution exists with colors: [1, 2, 2, 3]
+
+==============================
+
+Graph 2 with 2 colors:
+Solution exists with colors: [1, 2, 1, 2]
+
+==============================
+
+Graph 3 with 2 colors:
+Solution does not exist with 2 colors.
+>>> 
+= RESTART: C:/Users/student/AppData/Local/Programs/Python/Python312/task8nqueenproblem.py
+Graph 1 with 3 colors:
+Solution exists with colors: [1, 2, 2, 3]
+
+==============================
+
+Graph 2 with 2 colors:
+Solution exists with colors: [1, 2, 1, 2]
+
+==============================
+
+Graph 3 with 2 colors:
+Solution does not exist with 2 colors.
+>>> 
+== RESTART: C:/Users/student/Desktop/gani/task8nqueenproblem.py =
+Graph 1 with 3 colors:
+Solution exists with colors: [1, 2, 2, 3]
+
+==============================
+
+Graph 2 with 2 colors:
+Solution exists with colors: [1, 2, 1, 2]
+
+==============================
+
+Graph 3 with 2 colors:
+Solution does not exist with 2 colors.
